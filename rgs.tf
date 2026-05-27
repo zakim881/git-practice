0fb52c8e5b2a7606b48fc988f59f6f9ceb293f68
Zaki <zakim881@gmail.com>
2026-05-27 10:27:50 +0530

    Add Azure resource group configuration
    rg1 creation

--- rgs.tf
+++ rgs.tf
@@ -11,3 +11,8 @@ provider "azurerm" {
   resource_provider_registrations = "none" # This is only required when the User, Service Principal, or Identity running Terraform lacks the permissions to register Azure Resource Providers.
   features {}
 }
+
+resource "azurerm_resource_group" "example" {
+  name     = "rg1"
+  location = "West Europe"
+}
